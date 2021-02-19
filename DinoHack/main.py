bccf3c51009b93031e39c5315983f8067e012cf2
ToymanShikhar <shikharverma043@gmail.com>
2021-02-20 00:14:50 +0530

    Add Dino Project

--- DinoHack/main.py
+++ DinoHack/main.py
@@ -1,5 +1,34 @@
-import tkinter
+import pyautogui
+from PIL import Image
+import pyscreenshot as ImageGrab
+import time
 
-window = tkinter.Tk()
 
-window.mainloop()
+def isCollide(data):
+    for i in range(475, 580):
+        for j in range(525, 555):
+            if data[i, j] < 200:
+                return True
+    return False
+
+
+def hit(key):
+    pyautogui.press(key)
+
+
+if __name__ == "__main__":
+    print("Hey boi, game about to start in 3 seconds")
+    time.sleep(3)
+    hit("up")
+    while True:
+        image = ImageGrab.grab().convert('L')
+        data = image.load()
+        if isCollide(data):
+            hit("up")
+
+    # image = ImageGrab.grab().convert('L')
+    # data = image.load()
+    # for i in range(400, 540):
+    #     for j in range(500, 560):
+    #         data[i, j] = 0
+    # image.show()
